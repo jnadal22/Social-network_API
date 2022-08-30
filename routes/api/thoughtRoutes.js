64de21b56d25843f1c592b89a87efc5ecd32fba0
Jordan Nadal <nadal_jordan@yahoo.com>
2022-08-30 08:47:34 -0500

    worked on routes and controllers

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,2 +1,42 @@
 const router = require('express').Router();
 
+const { thoughts, users } = require('../models');
+
+module.exports = {
+    getThoughts(req, res) {
+      thoughts.find()
+        .then((thoughts) => res.json(thoughts))
+        .catch((err) => res.status(500).json(err));
+    },
+    getSingleThought(req, res) {
+      thoughts.findOne({ _id: req.params.thougthId })
+        .then((thought) =>
+          !thought
+            ? res.status(404).json({ message: 'No thought with that ID' })
+            : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    // create a new post
+    createThought(req, res) {
+      thoughts.create(req.body)
+        .then((thought) => {
+          return user.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: thought._id } },
+            { new: true }
+          );
+        })
+        .then((user) =>
+          !user
+            ? res
+                .status(404)
+                .json({ message: 'thought created, but found no user with that ID' })
+            : res.json('Created the thought')
+        )
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    },
+  };
